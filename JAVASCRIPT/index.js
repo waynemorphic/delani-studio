9079c9f21439260b77bf244c24bf2fdc24f68cf7
waynemorphic <wayneklyde@gmail.com>
2022-03-13 01:41:13 +0300

    Added jquery functions to toggle through descriptions on what we do section

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,3 +1,22 @@
 console.log('This is your master');
 let d = document;
 
+$(document).ready(function(){
+    $('#flip-design').click(function(){
+        $('#panel-design').fadeToggle('slow','linear');        
+    });
+});
+
+$(document).ready(function(){
+    $('#flip-dev').click(function(){
+        $('#panel-dev').fadeToggle('slow', 'linear');        
+    });
+});
+
+$(document).ready(function(){
+    $('#flip-product').click(function(){
+        $('#panel-product').fadeToggle('slow','linear');        
+    });
+});
+event.preventDefault();
+
