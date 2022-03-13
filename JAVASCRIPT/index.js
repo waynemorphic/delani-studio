c92adb2122660b05b68123c61037f642d736d38d
waynemorphic <wayneklyde@gmail.com>
2022-03-13 22:27:23 +0300

    Made the forms on contact region responsive

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -20,6 +20,38 @@ $(document).ready(function(){
         $('#panel-product').fadeToggle('slow', 'linear');        
     });
 });
-event.preventDefault();
+
 
 //Images hover effect
+$(document).ready(function(){
+    $('.child-image-1').hover(function(){
+        $('.child-image-1').show();
+    })
+})
+
+
+//Business Logic
+
+$(document).ready(function(){
+    $('#clickbtn').click(function(event){
+        submitForm(event);
+});
+});
+
+function submitForm(event){
+
+    event.preventDefault();
+
+    let mailText = document.querySelector('#message').value;
+    let userId = document.querySelector('#userName').value;
+    let emailAddress = document.querySelector('#userMail').value;
+    
+    ;
+
+    if (mailText == '' && emailAddress == '') {
+        alert ('Enter a valid email address and type a message in the text box')
+    }
+    if (mailText !== '') {
+        alert ((userId) + ' we have recieved your message. Thank you for reaching out to us')
+    }
+} 
